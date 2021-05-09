1ff672434e609960d68688be1243b743996486d6
Han Cen <hi@chamburr.com>
2021-05-09 15:17:21 +0800

    fix: use relative path

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
     })
 }
 
