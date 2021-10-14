d16923e64549aadb2b667aa67df869a45c96c073
Han Cen <hi@chamburr.com>
2021-10-14 15:42:26 +0800

    chore: update email address

--- assets/js/about.js
+++ assets/js/about.js
@@ -5,7 +5,7 @@ $('#contact-form').submit(function (event) {
     var title = encodeURIComponent('About Shooty Game...')
     var content = encodeURIComponent('Hi there,\n\n' + message + '\n\nRegards,\n' + name)
 
-    window.open('mailto:hi@chamburr.xyz?subject=' + title + '&body=' + content,'_blank')
+    window.open('mailto:hi@chamburr.com?subject=' + title + '&body=' + content,'_blank')
 
     event.preventDefault()
     event.stopPropagation()
